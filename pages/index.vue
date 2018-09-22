787d42fe9a71241b614fd1d5afe89b27acc73e1c
HANATANI Takuma <mail@potato4d.me>
2018-09-22 22:06:41 +0900

    create

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <div id="content">
+      Hello
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 16px;
+}
+
+#content {
+  width: 512px;
+  height: 512px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  position: relative;
+  background: url('~/assets/background.jpg')
+}
+
+#content::after {
+  width: 100%;
+  height: 100%;
+  content: '';
+  display: block;
+  position: absolute;
+  background: rgba(0,0,0,0.2);
+}
+</style>
